37cf976dcdb55bc980e0ddfbf216be10277a8eaa
SokolovDADeveloper <sokolovdadeveloper@gmail.com>
2021-02-25 11:14:17 +0300

    ref: refactoring 1

--- opener_cont_script.js
+++ opener_cont_script.js
@@ -115,257 +115,291 @@
 })()
 
 function opener_cont_script(extOptions) {
-  const mt = window.Mousetrap
 
-  controller(extOptions)
+  setKeys(extOptions, setKeysSearchEngines, setKeysUndefinedPages)
 
-  function controller(extOptions) {
-    setKeysBehaviorForPage(extOptions, setKeysSearchEngines, setKeysUndefinedPages)
+  function setKeys(extOptions, setKeysSearchEngines, setKeysUndefinedPages) {
 
-    function setKeysBehaviorForPage(extOptions, setKeysSearchEngines, setKeysUndefinedPages) {
+    const {searchEnginesSettings} = extOptions
 
-      const {searchEnginesSettings} = extOptions
+    const currentUrl = window.location.href
 
-      const currentUrl = window.location.href
+    for (const candidate of searchEnginesSettings) {
+      const regex = new RegExp(candidate.activatePattern, 'gi')
+      const match = regex.test(currentUrl)
 
-      for (const candidate of searchEnginesSettings) {
-        const regex = new RegExp(candidate.activatePattern, 'gi')
-        const match = regex.test(currentUrl)
-
-        if (match && candidate.active) {
-          return setKeysSearchEngines(candidate, extOptions)
-        }
-
-      }
-
-      setKeysUndefinedPages(extOptions)
-    }
-
-    function setKeysSearchEngines(engineSettings, extOptions) {
-      setCtrlAllow_SE(engineSettings, extOptions)
-
-      function setCtrlAllow_SE(engineSettings, extOptions) {
-
-        const {
-          SELECTORS
-        } = engineSettings
-
-        const multiOpenOpts = {
-          numOpenResults: extOptions.numOpenResults,
-          openLinksStartDelay: extOptions.openLinksStartDelay
-        }
-
-        mt.bind('ctrl+up', function (e) {
-          openFewResults(SELECTORS, multiOpenOpts)
-          return false;
-        });
-
-        mt.bind('ctrl+right', function (e) {
-          openFirstResultNewTab(SELECTORS)
-          return false;
-        });
-
-         mt.bind('ctrl+down', function (e) {
-           openFirstResultInActiveTab(SELECTORS)
-           return false;
-         });
-        mt.bind('ctrl+left', function (e) {
-          removeFirstResultElem(SELECTORS)
-          return false;
-        });
-
-        mt.bind('ctrl+shift+right', function (e) {
-          goToNextTab()
-          return false;
-        });
-      }
-
-      /***   Actions Search Pages  ***/
-      function openFewResults(SELECTORS, multiOpenOpts) {
-        openFirstResultInActiveTab(SELECTORS)
-
-        let {
-          openLinksStartDelay,
-          numOpenResults: conter
-        } = multiOpenOpts
-
-        conter = conter - 1
-
-        while(conter) {
-          setTimeout( () => {
-            openFirstResultNewTab(SELECTORS)
-          } , conter * openLinksStartDelay )
-          conter--
-        }
-      }
-
-      function openFirstResultNewTab(SELECTORS) {
-
-        const data = getDataForOpen(SELECTORS)
-        if (data.error) {
-          console.error(data.reason)
-          return
-        }
-
-        openUrlInNewTab(data.url, {active: false})
-        data.resultElem.remove()
-
-      }
-
-      function openFirstResultInActiveTab(SELECTORS) {
-        const data = getDataForOpen(SELECTORS)
-        if (data.error) {
-          console.error(data.reason)
-          return
-        }
-
-        openUrlInNewTab(data.url, {active: true})
-        data.resultElem.remove()
-      }
-
-      function removeFirstResultElem(SELECTORS) {
-        const data = getDataForOpen(SELECTORS)
-        if (data.error) {
-          console.error(data.reason)
-          return
-        }
-        data.resultElem.remove()
-      }
-       // actions-search-pages
-      /***   Helpers Search Pages  ***/
-      function openUrlInNewTab(url, options = {}) {
-
-        const {
-          active = true
-        } = options
-
-        chrome.runtime.sendMessage(
-          {
-            command: 'openUrlNewTab',
-            payload: {
-              url,
-              active
-            }
-          },
-          () => {
-          }
-        )
-      }
-
-      function getDataForOpen(SELECTORS) {
-
-        const {
-          resultsContainerSelector,
-          resultElemSelector,
-          resultLinkSelector
-        } = SELECTORS
-
-        try {
-          const resultElem = getElementBySelector(resultElemSelector, resultsContainerSelector)
-          const linkElem = getElementBySelector(resultLinkSelector, resultElem)
-          const url = linkElem.href
-
-          return {
-            error: false,
-            resultElem,
-            url
-          }
-
-        } catch (err) {
-           return {
-             error: true,
-             reason: err
-           }
-        }
-      }
-      // helpers-search-pages
-    }
-
-    function setKeysUndefinedPages(extOptions) {
-      setCtrlAllow(extOptions)
-
-      function setCtrlAllow(extOptions) {
-
-        const {scrollByPx} = extOptions
-
-        mt.bind('ctrl+up', function (e) {
-          window.scrollBy(0,-scrollByPx);
-          return false;
-        });
-
-        mt.bind('ctrl+right', function (e) {
-          closeTab()
-          return false;
-        });
-
-        mt.bind('ctrl+down', function (e) {
-          window.scrollBy(0,scrollByPx);
-          return false;
-        });
-
-        mt.bind('ctrl+left', function (e) {
-          goToNextTab()
-          return false;
-        });
+      if (match && candidate.active) {
+        return setKeysSearchEngines(candidate, extOptions)
       }
 
     }
 
-    function closeTab() {
-      chrome.runtime.sendMessage(
-        {
-          command: 'closeTab',
-        },
-        () => {  }
-      );
+    setKeysUndefinedPages(extOptions)
+  }
+}
+
+function setKeysSearchEngines(engineSettings, extOptions) {
+  setCtrlAllow_SE(engineSettings, extOptions)
+  setShiftAllow_SE(engineSettings, extOptions)
+
+  function setCtrlAllow_SE(engineSettings, extOptions) {
+
+    const {
+      SELECTORS
+    } = engineSettings
+
+    const multiOpenOpts = {
+      numOpenResults: extOptions.numOpenResults,
+      openLinksStartDelay: extOptions.openLinksStartDelay
     }
 
-    function goToNextTab() {
-      chrome.runtime.sendMessage(
-        {
-          command: 'shiftTab',
-          payload: {
-            direction: "next"
-          }
-        },
-        () => {  }
-      );
+    window.Mousetrap.bind('ctrl+up', function (e) {
+      openFewResults(SELECTORS, multiOpenOpts)
+      return false;
+    });
+
+    window.Mousetrap.bind('ctrl+right', function (e) {
+      openFirstResultNewTab(SELECTORS)
+      return false;
+    });
+
+    window.Mousetrap.bind('ctrl+down', function (e) {
+      openFirstResultInActiveTab(SELECTORS)
+      return false;
+    });
+
+    window.Mousetrap.bind('ctrl+left', function (e) {
+      removeFirstResultElem(SELECTORS)
+      return false;
+    });
+  }
+
+  function setShiftAllow_SE(engineSettings, extOptions) {
+
+    const {
+      SELECTORS
+    } = engineSettings
+
+    window.Mousetrap.bind('shift+right', function (e) {
+      goToNextTab()
+      return false;
+    });
+
+    window.Mousetrap.bind('shift+left', function (e) {
+      goToPrevTab()
+      return false;
+    });
+  }
+
+  /***   Actions Search Pages  ***/
+  function openFewResults(SELECTORS, multiOpenOpts) {
+    openFirstResultInActiveTab(SELECTORS)
+
+    let {
+      openLinksStartDelay,
+      numOpenResults: conter
+    } = multiOpenOpts
+
+    conter = conter - 1
+
+    while(conter) {
+      setTimeout( () => {
+        openFirstResultNewTab(SELECTORS)
+      } , conter * openLinksStartDelay )
+      conter--
+    }
+  }
+
+  function openFirstResultNewTab(SELECTORS) {
+
+    const data = getDataForOpen(SELECTORS)
+    if (data.error) {
+      console.error(data.reason)
+      return
     }
 
-    function getElementBySelector(selector, context) {
+    openUrlInNewTab(data.url, {active: false})
+    data.resultElem.remove()
 
-      const extensionName = '[web keys surfer]'
+  }
 
-      let contextElem
+  function openFirstResultInActiveTab(SELECTORS) {
+    const data = getDataForOpen(SELECTORS)
+    if (data.error) {
+      console.error(data.reason)
+      return
+    }
 
-      if (isElement(context)) {
-        contextElem = context
-      } else {
-        contextElem = document.querySelector(context)
-        if (!contextElem) {
-          console.error(`
+    openUrlInNewTab(data.url, {active: true})
+    data.resultElem.remove()
+  }
+
+  function removeFirstResultElem(SELECTORS) {
+    const data = getDataForOpen(SELECTORS)
+    if (data.error) {
+      console.error(data.reason)
+      return
+    }
+    data.resultElem.remove()
+  }
+  // actions-search-pages
+  /***   Helpers Search Pages  ***/
+  function openUrlInNewTab(url, options = {}) {
+
+    const {
+      active = true
+    } = options
+
+    chrome.runtime.sendMessage(
+      {
+        command: 'openUrlNewTab',
+        payload: {
+          url,
+          active
+        }
+      },
+      () => {
+      }
+    )
+  }
+
+  function getDataForOpen(SELECTORS) {
+
+    const {
+      resultsContainerSelector,
+      resultElemSelector,
+      resultLinkSelector
+    } = SELECTORS
+
+    try {
+      const resultElem = getElementBySelector(resultElemSelector, resultsContainerSelector)
+      const linkElem = getElementBySelector(resultLinkSelector, resultElem)
+      const url = linkElem.href
+
+      return {
+        error: false,
+        resultElem,
+        url
+      }
+
+    } catch (err) {
+      return {
+        error: true,
+        reason: err
+      }
+    }
+  }
+  // helpers-search-pages
+}
+
+function setKeysUndefinedPages(extOptions) {
+  setCtrlAllow(extOptions)
+  setShiftlAllow(extOptions)
+
+  function setCtrlAllow(extOptions) {
+
+    const {scrollByPx} = extOptions
+
+    window.Mousetrap.bind('ctrl+up', function (e) {
+      window.scrollBy(0,-scrollByPx);
+      return false;
+    });
+
+    window.Mousetrap.bind('ctrl+right', function (e) {
+      closeTab()
+      return false;
+    });
+
+    window.Mousetrap.bind('ctrl+down', function (e) {
+      window.scrollBy(0,scrollByPx);
+      return false;
+    });
+
+    window.Mousetrap.bind('ctrl+left', function (e) {
+      goToNextTab()
+      return false;
+    });
+  }
+
+  function setShiftlAllow(extOptions) {
+
+    window.Mousetrap.bind('shift+right', function (e) {
+      goToNextTab()
+      return false;
+    });
+
+    window.Mousetrap.bind('shift+left', function (e) {
+      goToPrevTab()
+      return false;
+    });
+  }
+}
+
+function closeTab() {
+  chrome.runtime.sendMessage(
+    {
+      command: 'closeTab',
+    },
+    () => {  }
+  );
+}
+
+function goToNextTab() {
+  chrome.runtime.sendMessage(
+    {
+      command: 'shiftTab',
+      payload: {
+        direction: "next"
+      }
+    },
+    () => {  }
+  );
+}
+
+function goToPrevTab() {
+  chrome.runtime.sendMessage(
+    {
+      command: 'shiftTab',
+      payload: {
+        direction: "prev"
+      }
+    },
+    () => {  }
+  );
+}
+
+function getElementBySelector(selector, context) {
+
+  const extensionName = '[web keys surfer]'
+
+  let contextElem
+
+  if (isElement(context)) {
+    contextElem = context
+  } else {
+    contextElem = document.querySelector(context)
+    if (!contextElem) {
+      console.error(`
         ${extensionName}
         Not found context by selector: ${contextElem}`)
-          return null
-        }
-      }
+      return null
+    }
+  }
 
-      const element = contextElem.querySelector(selector)
-      if (!element) {
-        console.error(`
+  const element = contextElem.querySelector(selector)
+  if (!element) {
+    console.error(`
         ${extensionName}
         Not found element by selector: ${selector}
         Context:`)
-        console.error(contextElem)
-        return null
-      }
-
-      return element
-
-      function isElement(element) {
-        return element instanceof Element || element instanceof HTMLDocument;
-      }
-
-    }
+    console.error(contextElem)
+    return null
   }
+
+  return element
+
+  function isElement(element) {
+    return element instanceof Element || element instanceof HTMLDocument;
+  }
+
 }
