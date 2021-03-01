057c097197ab68b6febb515d0a158ef2cb97b7b3
SokolovDADeveloper <sokolovdadeveloper@gmail.com>
2021-03-01 13:23:51 +0300

    fix manifest

---
+++ closerByMouse.js
@@ -0,0 +1,44 @@
+if (window.closerByMouse === undefined) {
+  const recieveRightClick = (function () {
+    let counter = 0
+    let lastTime = new Date()
+    let tabRemoveAlreadyRequested = false
+
+    return function (e) {
+      if (e.which !== 3) {
+        return
+      }
+
+      counter++
+      setTimeout( () => {
+        if (counter) {
+          counter--
+          console.log('\x1b[36m%s\x1b[0m', "counter = ", counter)
+        }
+      }, 500)
+
+      let thisTime = new Date()
+      let timeDiff = thisTime - lastTime
+      lastTime = thisTime
+
+      if ((counter === 2 && timeDiff >= 50) || counter > 2) {
+        if (!tabRemoveAlreadyRequested) {
+          tabRemoveAlreadyRequested = true
+          chrome.runtime.sendMessage({closeTab: true})
+        }
+      }
+    }
+  }());
+
+  const interval = setInterval(function () {
+    if (document.body) {
+      document.onmouseup = function (e) {
+        recieveRightClick(e);
+      }
+    }
+
+    clearInterval(interval)
+  }, 100)
+
+  window.closerByMouse = true
+}
